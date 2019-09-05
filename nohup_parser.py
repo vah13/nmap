660da16a2a8be151247b5f576303ad9422f4d7a4
vah_13 <vvvaagn@gmail.com>
2019-09-05 15:56:41 +0300

    Update nohup_parser.py

--- nohup_parser.py
+++ nohup_parser.py
@@ -1,5 +1,8 @@
+import requests
+
 report_file = r"C:\Users\vava\Desktop\research\att\search_services\nohup.out"
 matches = "@vah_13 status code: 302"
+_scan_content = ""
 scan_path = "/services/"
 _except = "sbcglobal"
 
@@ -8,6 +11,12 @@ def get_domain_from_scan(str):
     if " " in str and "(" in str:
         str = str.split(" ")[0]
     return str
+
+
+def get_content_from_uri(uri):
+    _content = requests.get(uri, verify=False, allow_redirects=True).content
+    if _scan_content in _content:
+        return "OK"
 
 
 with open(report_file, 'r') as f:
@@ -19,4 +28,6 @@ for line in lines:
     if matches in line:
         domain = get_domain_from_scan(lines[i - 7].strip())
         if _except not in domain:
-            print "https://" + domain + scan_path
+            URI =  "https://" + domain + scan_path
+            print "[*] Start scan {0}".format(URI)
+            print get_content_from_uri(URI)
